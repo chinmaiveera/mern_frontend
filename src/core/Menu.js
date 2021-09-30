665f4d67d890d200bf1c5004373d446d64f09907
chinmai_veera <chinmaiveera4@gmail.com>
2021-09-30 12:39:41 +0530

    fist commit

---
+++ src/core/Menu.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import { isAuthenticated, signout } from '../auth/helper';
+
+const Menu = ({ history }) => {
+
+    const currentTab = (history, path) => {
+        if (history.location.pathname === path) {
+            return { color: "#2ecc72" };
+        } else {
+            return { color: "#ffffff" };
+        }
+    };
+
+
+    return (
+        <div>
+            <ul className="nav nav-tabs db-dark">
+                <li className="nav-items">
+                    <Link style={currentTab(history, "/")} className="nav-link" to="/">
+                        Home
+                    </Link>
+                </li>
+                <li className="nav-items">
+                    <Link style={currentTab(history, "/cart")} className="nav-link" to="/cart">
+                        Cart
+                    </Link>
+                </li>
+                {isAuthenticated() && isAuthenticated().user.role === 0 && (
+                    <Link style={currentTab(history, "/user/dashboard")} className="nav-link" to="/user/dashboard">
+                        Dashboard
+                    </Link>
+                )}
+                {isAuthenticated() && isAuthenticated().user.role === 1 && (
+                    <Link style={currentTab(history, "/admin/dashboard")} className="nav-link" to="/admin/dashboard">
+                        Dashboard
+                    </Link>
+                )}
+                {!isAuthenticated() && (
+                    <li className="nav-item">
+                        <Link style={currentTab(history, "/signup")} className="nav-link" to="/signup">
+                            Sign Up
+                        </Link>
+                    </li>
+                )}
+
+                {!isAuthenticated() && (
+                    <li className="nav-item">
+                        <Link style={currentTab(history, "/signin")} className="nav-link" to="/signin">
+                            Sign In
+                        </Link>
+                    </li>
+                )}
+                {isAuthenticated() && (
+                    <li className="nav-item">
+                        <span
+                            className="nav-link text-warning"
+                            onClick={() => {
+                                signout(() => {
+                                    history.push("/");
+                                });
+                            }}
+                        >
+                            Signout
+                        </span>
+                    </li>
+                )}
+            </ul>
+        </div>
+    )
+}
+
+
+
+export default withRouter(Menu);
